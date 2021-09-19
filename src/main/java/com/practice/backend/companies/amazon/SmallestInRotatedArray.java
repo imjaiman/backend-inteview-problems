2a9be7435ea978a6a03dae9e349da552f38916c3
Rahul Jaiman <40609753+imjaiman@users.noreply.github.com>
2021-09-20 00:25:27 +0530

    Update SmallestInRotatedArray.java

--- src/main/java/com/practice/backend/companies/amazon/SmallestInRotatedArray.java
+++ src/main/java/com/practice/backend/companies/amazon/SmallestInRotatedArray.java
@@ -1,13 +1,6 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2020 All Rights Reserved.
- */
+
 package com.practice.backend.companies.amazon;
 
-/**
- * @author rahul.jaiman
- * @version $Id: SmallestInRotatedArray.java, v 0.1 2020-05-23 14:40 rahul.jaiman Exp $$
- */
 public class SmallestInRotatedArray {
     public static void main(String[] args) {
         int[] arr = {5, 6, 1, 2, 3, 4};
