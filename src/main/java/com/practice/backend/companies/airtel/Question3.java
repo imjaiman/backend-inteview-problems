cc33ea9ebdd80e41097116e399225238f6298f04
Rahul Jaiman <40609753+imjaiman@users.noreply.github.com>
2021-09-20 00:25:05 +0530

    Update Question3.java

--- src/main/java/com/practice/backend/companies/airtel/Question3.java
+++ src/main/java/com/practice/backend/companies/airtel/Question3.java
@@ -1,13 +1,6 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2020 All Rights Reserved.
- */
+
 package com.practice.backend.companies.airtel;
 
-/**
- * @author rahul.jaiman
- * @version $Id: Question3.java, v 0.1 2020-05-26 21:42 rahul.jaiman Exp $$
- */
 public class Question3 {
     public static void main(String[] args) {
 
